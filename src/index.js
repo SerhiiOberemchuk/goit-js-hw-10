9f58ae4ec8de9c4cf9b40ed7d07814c51279ffe6
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-10-22 14:53:58 +0200

    05

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 const selectBreed = document.querySelector('.breed-select');
 
-// import SlimSelect from 'slim-select';
-// new SlimSelect({
-//   select: '.breed-select',
-// });
+import SlimSelect from 'slim-select';
 
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
 fetchBreeds(BASE_URL).then(resp => {
   const markap = resp.data
     .map(({ id, name }) => `<option value="${id}">${name}</option>`)
     .join('');
-  selectBreed.innerHTML = markap;
+  selectBreed.innerHTML = `<option data-placeholder="true"></option>${markap}`;
+  new SlimSelect({
+    select: selectBreed,
+    settings: {
+      placeholderText: 'Please choose your favorite cat',
+    },
+  });
 });
 
 selectBreed.addEventListener('change', fetchCatByBreed);
