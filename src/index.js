b72f013312ffef6b230b27847d4c2923dd36de82
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-10-22 14:11:41 +0200

    04

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 const selectBreed = document.querySelector('.breed-select');
-import SlimSelect from 'slim-select';
+
+// import SlimSelect from 'slim-select';
 // new SlimSelect({
 //   select: '.breed-select',
 // });
+
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
 fetchBreeds(BASE_URL).then(resp => {
   const markap = resp.data
