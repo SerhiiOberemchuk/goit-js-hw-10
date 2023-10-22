bacb124a41b34647b90ce457363ab8a08166f4e1
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-10-22 12:08:04 +0200

    01

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,47 +1,49 @@
 import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 axios.defaults.headers.common['x-api-key'] =
   'live_LiSWxPZjX4QMvlIZoWOSNRATcDjkNrUPppORYPMIKidSRLdDmMw4NhMmmVdPdXGN';
 
-const fetchBreeds = urlCat => {
-  // const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
-  return axios.get(urlCat).then(resp => {
-    return resp;
-  });
-  // .catch(error => {
-  //   return error;
-  // });
-};
 const boxCatInfo = document.querySelector('.cat-info');
+
 const loaderBox = document.querySelector('.loader');
 const errorText = document.querySelector('.error');
 errorText.style.display = 'none';
 loaderBox.style.display = 'none';
 
+const fetchBreeds = urlCat => {
+  return axios
+    .get(urlCat)
+    .then(resp => {
+      return resp;
+    })
+    .catch(error => {
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    });
+};
+
 const fetchCatByBreed = event => {
   const breedId = event.target.value;
-  errorText.style.display = 'none';
+  boxCatInfo.style.display = 'none';
   loaderBox.style.display = 'block';
   const BASE_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
   return axios
     .get(BASE_URL)
     .then(resp => {
-      console.log(resp);
-      boxCatInfo.style = 'display: flex';
+      boxCatInfo.style.display = 'flex';
       loaderBox.style.display = 'none';
       const dataCat = resp.data[0].breeds[0];
       const image = resp.data[0].url;
-      const markup = `<img src="${image}" alt="${dataCat.name}" width='500'>
+      const markup = `<img src="${image}" alt="${dataCat.name}" width='300' style="margin-top: 20px; margin-right: 20px;">
       <div>
         <h1>${dataCat.name}</h1>
         <p>${dataCat.description}</p>
-        <p>Temperament:${dataCat.temperament}</p>
+        <p><span style="font-weight: bold;">Temperament:</span>${dataCat.temperament}</p>
       </div>`;
 
       boxCatInfo.innerHTML = markup;
     })
     .catch(error => {
-      loaderBox.style.display = 'none';
-      errorText.style.display = 'block';
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
     });
 };
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 const selectBreed = document.querySelector('.breed-select');
-
-// const loaderBox = document.querySelector('.loader');
+import SlimSelect from 'slim-select';
 
 const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
-fetchBreeds(BASE_URL)
-  .then(resp => {
-    const markap = resp.data
-      .map(({ id, name }) => `<option value="${id}">${name}</option>`)
-      .join('');
-    selectBreed.innerHTML = markap;
-  })
-  .catch(error => {
-    errorText.style.display = 'inline';
-  });
+fetchBreeds(BASE_URL).then(resp => {
+  const markap = resp.data
+    .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+    .join('');
+  selectBreed.innerHTML = markap;
+  // new SlimSelect(selectBreed, {
+  //   placeholder: 'Custom Placeholder Text',
+  // });
+  selectBreed.slim.open();
+});
 
 selectBreed.addEventListener('change', fetchCatByBreed);
